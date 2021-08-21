dacc1c4a3f6caac95641c302eec79cad194d589b
root <13120612730@163.com>
2021-08-22 02:23:08 +0800

    test: lab3 passed 31 of 33 tests

--- sponge/libsponge/tcp_sender.cc
+++ sponge/libsponge/tcp_sender.cc
@@ -34,44 +34,115 @@ void TCPSender::fill_window() {
         // std::cout << "_stream.buffer_empty() = " << _stream.buffer_empty() << std::endl;
         // std::cout << "next_seqno_absolute() = " << next_seqno_absolute() << std::endl;
         if (!_stream.buffer_empty()) {
-            auto unread_size = _stream.buffer_size()-_stream.remaining_capacity();
-            auto size = unread_size < _window_size?unread_size:_window_size;
-            size = size < TCPConfig::MAX_PAYLOAD_SIZE?size:TCPConfig::MAX_PAYLOAD_SIZE;
-            auto str = _stream.read(size);
-            if (next_seqno_absolute()) {
-                if (_stream.buffer_empty() && _stream.input_ended()) {
-                    seg = make_segment(next_seqno(),false,true,str);
+            if (next_seqno_absolute()) {    //SYN待发送
+                auto unread_size = _stream.buffer_size();
+                if (unread_size < _window_size) {
+                    auto size = unread_size < TCPConfig::MAX_PAYLOAD_SIZE?unread_size:TCPConfig::MAX_PAYLOAD_SIZE;
+                    auto str = _stream.read(size);
+                    if (_stream.input_ended()) {
+                        seg = make_segment(next_seqno(),false,true,str);
+                        _segments_out.push(seg);
+                        Out_Segment out_segment{seg,next_seqno_absolute(),_initial_retransmission_timeout};
+                        out_segments.push_back(out_segment);
+                        _window_size -= seg.length_in_sequence_space();
+                        _bytes_in_flight += seg.length_in_sequence_space();
+                        _next_seqno += seg.length_in_sequence_space();
+                        break;
+                    } else {
+                        seg = make_segment(next_seqno(),false,false,str);
+                        _segments_out.push(seg);
+                        Out_Segment out_segment{seg,next_seqno_absolute(),_initial_retransmission_timeout};
+                        out_segments.push_back(out_segment);
+                        _window_size -= seg.length_in_sequence_space();
+                        _bytes_in_flight += seg.length_in_sequence_space();
+                        _next_seqno += seg.length_in_sequence_space();
+                    }
                 } else {
+                    auto size = _window_size < TCPConfig::MAX_PAYLOAD_SIZE?_window_size:TCPConfig::MAX_PAYLOAD_SIZE;
+                    auto str = _stream.read(size);
                     seg = make_segment(next_seqno(),false,false,str);
+                    _segments_out.push(seg);
+                    Out_Segment out_segment{seg,next_seqno_absolute(),_initial_retransmission_timeout};
+                    out_segments.push_back(out_segment);
+                    _window_size -= seg.length_in_sequence_space();
+                    _bytes_in_flight += seg.length_in_sequence_space();
+                    _next_seqno += seg.length_in_sequence_space();
                 }
-            } else {
-                if (_stream.buffer_empty() && _stream.input_ended()) {
-                    seg = make_segment(next_seqno(),true,true,str);
+            } else {    //SYN已发送
+                auto unread_size = _stream.buffer_size()-_stream.remaining_capacity();
+                if (unread_size < _window_size - 1) {
+                    if (unread_size <= TCPConfig::MAX_PAYLOAD_SIZE) {
+                        auto str = _stream.read(unread_size);
+                        seg = make_segment(next_seqno(),true,true,str);
+                        _segments_out.push(seg);
+                        Out_Segment out_segment{seg,next_seqno_absolute(),_initial_retransmission_timeout};
+                        out_segments.push_back(out_segment);
+                        _window_size -= seg.length_in_sequence_space();
+                        _bytes_in_flight += seg.length_in_sequence_space();
+                        _next_seqno += seg.length_in_sequence_space();
+                        break;
+                    } else {
+                        auto str = _stream.read(TCPConfig::MAX_PAYLOAD_SIZE);
+                        seg = make_segment(next_seqno(),true,false,str);
+                        _segments_out.push(seg);
+                        Out_Segment out_segment{seg,next_seqno_absolute(),_initial_retransmission_timeout};
+                        out_segments.push_back(out_segment);
+                        _window_size -= seg.length_in_sequence_space();
+                        _bytes_in_flight += seg.length_in_sequence_space();
+                        _next_seqno += seg.length_in_sequence_space();
+                    }
+                } else if (unread_size < _window_size) {
+                        auto size = unread_size < TCPConfig::MAX_PAYLOAD_SIZE?unread_size:TCPConfig::MAX_PAYLOAD_SIZE;
+                        auto str = _stream.read(size);
+                        seg = make_segment(next_seqno(),true,false,str);
+                        _segments_out.push(seg);
+                        Out_Segment out_segment{seg,next_seqno_absolute(),_initial_retransmission_timeout};
+                        out_segments.push_back(out_segment);
+                        _window_size -= seg.length_in_sequence_space();
+                        _bytes_in_flight += seg.length_in_sequence_space();
+                        _next_seqno += seg.length_in_sequence_space();
                 } else {
-                    seg = make_segment(next_seqno(),true,false,str);
+                        auto size = _window_size - 1 > TCPConfig::MAX_PAYLOAD_SIZE?TCPConfig::MAX_PAYLOAD_SIZE:_window_size - 1;
+                        auto str = _stream.read(size);
+                        seg = make_segment(next_seqno(),true,false,str);
+                        _segments_out.push(seg);
+                        Out_Segment out_segment{seg,next_seqno_absolute(),_initial_retransmission_timeout};
+                        out_segments.push_back(out_segment);
+                        _window_size -= seg.length_in_sequence_space();
+                        _bytes_in_flight += seg.length_in_sequence_space();
+                        _next_seqno += seg.length_in_sequence_space();
                 }
             } 
-            _segments_out.push(seg);
-            Out_Segment out_segment{seg,next_seqno_absolute(),_initial_retransmission_timeout};
-            out_segments.push_back(out_segment);
-            _window_size -= seg.length_in_sequence_space();
-            _bytes_in_flight += seg.length_in_sequence_space();
-            _next_seqno += seg.length_in_sequence_space();
+
         } else {
             if (!next_seqno_absolute()) {
-                if (_stream.input_ended()) {
+                if (_stream.input_ended() && _window_size >= 2) {
                     seg = make_segment(next_seqno(),true,true,"");
+                    _segments_out.push(seg);
+                    Out_Segment out_segment{seg,next_seqno_absolute(),_initial_retransmission_timeout};
+                    out_segments.push_back(out_segment);
+                    _window_size -= seg.length_in_sequence_space();
+                    _bytes_in_flight += seg.length_in_sequence_space();
+                    _next_seqno += 2;
+                    break;
                 } else {
                     seg = make_segment(next_seqno(),true,false,"");
+                    _segments_out.push(seg);
+                    Out_Segment out_segment{seg,next_seqno_absolute(),_initial_retransmission_timeout};
+                    out_segments.push_back(out_segment);
+                    _window_size -= seg.length_in_sequence_space();
+                    _bytes_in_flight += seg.length_in_sequence_space();
+                    _next_seqno++;
                 }
-                _segments_out.push(seg);
-                Out_Segment out_segment{seg,next_seqno_absolute(),_initial_retransmission_timeout};
-                out_segments.push_back(out_segment);
-                _window_size -= seg.length_in_sequence_space();
-                _bytes_in_flight += seg.length_in_sequence_space();
-                _next_seqno++;
             } else if (_stream.input_ended()) {
                     seg = make_segment(next_seqno(),false,true,"");
+                    _segments_out.push(seg);
+                    Out_Segment out_segment{seg,next_seqno_absolute(),_initial_retransmission_timeout};
+                    out_segments.push_back(out_segment);
+                    _window_size -= seg.length_in_sequence_space();
+                    _bytes_in_flight += seg.length_in_sequence_space();
+                    _next_seqno++;
+                    break;
             } else {
                 break;
             }
@@ -89,29 +160,59 @@ void TCPSender::ack_received(const WrappingInt32 ackno, const uint16_t window_si
         auto nq = unwrap(ackno,_isn,_next_seqno);
         if ((*it).ackno() < nq) {
             _consecutive_retransmissions = 0;
-
-            _next_seqno = nq;
             _bytes_in_flight -= (*it).segment().length_in_sequence_space();
             it = out_segments.erase(it);
         } else {
+            if ((*it).ackno() > _next_seqno) {
+                _next_seqno = (*it).ackno();
+            }
             it++;
         }
     }
-    fill_window();
+    // fill_window();
 }
 
 //! \param[in] ms_since_last_tick the number of milliseconds since the last call to this method
 void TCPSender::tick(const size_t ms_since_last_tick) { 
+
+    auto oldest_seg = out_segments.begin();
+    // auto begin_timeout = (*oldest_seg).timeout();
     for (auto it = out_segments.begin();it != out_segments.end();it++) {
         // if (auto t = (*it).timeout();t > ms_since_last_tick) {
-        if ((*it).timeout() > ms_since_last_tick) {
-            (*it).timeout() -= ms_since_last_tick;
-        } else {
-            (*it).restrans_time()++;
-            _consecutive_retransmissions++;
-            (*it).timeout() = _initial_retransmission_timeout << (*it).restrans_time();
-            _segments_out.push((*it).segment());
-        }
+        // if ((*it).timeout() <= ms) {
+            // (*it).timeout() -= ms;
+        // } else if ((*it).timeout() == ms){
+        //     (*it).restrans_time()++;
+        //     _consecutive_retransmissions++;
+        //     (*it).timeout() = _initial_retransmission_timeout << (*it).restrans_time();
+        //     _segments_out.push((*it).segment());
+        // } else {
+        //     ms = ms_since_last_tick - (*it).timeout();
+        //     tick((*it).timeout());
+        //     tick(ms);
+        //     break;
+        // }
+        // } else {
+            if (((*it).restrans_time() > (*oldest_seg).restrans_time())) {
+                oldest_seg = it;
+            } else if ((*it).restrans_time() == (*oldest_seg).restrans_time()){
+                if  ((*it).timeout() < (*oldest_seg).timeout()) {
+                    oldest_seg = it;
+                }
+            }
+        // }
+    }
+    // bool a = begin_timeout == (*out_segments.begin()).timeout();   //第一个元素没变
+    // bool b = out_segments.begin() != oldest_seg;  //不是第一个元素
+    bool c = (*oldest_seg).timeout() <= ms_since_last_tick; 
+    
+    if (c) {
+        (*oldest_seg).restrans_time()++;
+        _consecutive_retransmissions++;
+        (*oldest_seg).timeout() = _initial_retransmission_timeout << (*oldest_seg).restrans_time();
+        _segments_out.push((*oldest_seg).segment());
+    } else {
+            (*oldest_seg).timeout() -= ms_since_last_tick;
     }
 }
 
@@ -140,7 +241,7 @@ TCPSegment TCPSender::make_segment(const WrappingInt32 seqno,const bool syn,cons
     // string &&s = move(const_cast<string &>(payload));
     // if (!payload.empty()) {
     if (!payload.empty()) {
-        seg.payload() = Buffer(string(payload));
+        seg.payload() = Buffer(string(move(payload)));
     }
     return seg; 
 }
